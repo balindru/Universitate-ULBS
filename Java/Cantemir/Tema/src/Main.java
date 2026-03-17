95e784e8fdf1f38348efd63dfc12e71a4ca6b3f1
BalindruCW <59843767+balindru@users.noreply.github.com>
2026-03-17 19:42:21 +0200

    tema

--- Java/Cantemir/Tema/src/Main.java
+++ Java/Cantemir/Tema/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Student{
 
@@ -26,19 +27,35 @@ public class Student{
                 ", Prenume: " + prenume +
                 ", Formatie: " + formatieDeStudiu;
     }
-}
+    @Override
+    public boolean equals(Object o) {
+        Student s = (Student) o;
+        return prenume.equals(s.prenume) &&
+                nume.equals(s.nume) &&
+                formatieDeStudiu.equals(s.formatieDeStudiu);
+    }
 
+    @Override
+    public int hashCode() {
+        return (prenume + nume + formatieDeStudiu).hashCode();
+    }
+}
 
 void main() {
     ArrayList<Student> list = new ArrayList<>();
     list.add(new Student(423, "Dumitru", "Vasile", "C22/1"));
-    list.add(new Student(284, "Alis", "Popa", "TI21/2"));
+    list.add(new Student(284, "Amir", "Tanasa", "T21/2"));
     list.add(new Student(130, "Ion", "Ionescu", "TI21/1"));
+    Set set = new HashSet();
+    set.addAll(list);
 
     System.out.println("Lista studenti:");
     for (Student s : list) {
         System.out.println(s);
     }
 
+
+    System.out.println(set.contains(new Student(284, "Amir", "Tanasa", "T21/2")));
+    System.out.println(set.contains(new Student(120, "Ion", "Ionescu", "TI21/1")));
 
 }
