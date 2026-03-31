fc41568b4ad8181361bcd5b129d7ded3b28f76f1
BalindruCW <59843767+balindru@users.noreply.github.com>
2026-03-31 20:01:07 +0300

    tema

--- Java/Cantemir/Tema/src/Main.java
+++ Java/Cantemir/Tema/src/Main.java
@@ -3,7 +3,7 @@ public class Student{
     private
     int numarMatricol;
     String prenume,nume,formatieDeStudiu;
-    Double nota;
+    Float nota;
     Student()
     {
         numarMatricol=0;
@@ -17,7 +17,7 @@ public class Student{
         prenume=pre;
         nume=num;
         formatieDeStudiu=form;
-        this.nota=0.0;
+        this.nota=0f;
     }
 
     public int getNumarMatricol() {
@@ -36,7 +36,11 @@ public class Student{
         return formatieDeStudiu;
     }
 
-    public void setNota(Double nota) { this.nota = nota; }
+    public Float getNota() {
+        return nota;
+    }
+
+    public void setNota(Float nota) { this.nota = nota; }
 
 
     public String toString() {
@@ -62,11 +66,26 @@ public class Student{
     public int hashCode() {
         return Integer.hashCode(numarMatricol);
     }
-
 }
 
+public static float gasesteNota(String prenume, String nume, Map<Integer, Student> tineri) {
+    HashMap<String, Student> gaseste = new HashMap<>();
+    for (Student s : tineri.values()) {
+        String cheie = s.getPrenume() + "-" + s.getNume();
+        gaseste.put(cheie, s);
+    }
+    String cheieCautare = prenume + "-" + nume;
+    Student gasit = gaseste.get(cheieCautare);
+    if (gasit != null && gasit.getNota() != null) {
+        return gasit.getNota().floatValue();
+    }
+    return 0.0f;
+}
 
 void main() {
+
+
+
     HashMap<Integer, Student> studenti = new HashMap<>();
     FileInputStream f = null;
     try {
@@ -93,7 +112,7 @@ void main() {
     while(sc.hasNext())
     {
         int nr=sc.nextInt();
-        Double nota=sc.nextDouble();
+        Float nota=sc.nextFloat();
         Student s = studenti.get(nr);
         if (s != null) {
             s.setNota(nota);
@@ -103,4 +122,9 @@ void main() {
         System.out.println(s);
     }
 
+    float notaM = gasesteNota("Bianca", "Popescu", studenti);
+    float notaN = gasesteNota("Ioan", "Popa", studenti);
+
+    System.out.println("Bianca " + notaM);
+    System.out.println("Ioan " + notaN);
 }
